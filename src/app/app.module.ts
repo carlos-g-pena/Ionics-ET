824bdac8b45d58ea6d3761e80443e4bcd83a6f12
Carlos Peña <cgabriel2303@gmail.com>
2024-08-29 20:18:05 -0400

    update login an main-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MenuComponent } from './menu/menu.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule, ],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  userName: string = '';
 
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth) {}
 
+  ngOnInit() {
+    this.afAuth.currentUser.then(user => {
+      if (user) {
+        this.userName = user.displayName || 'Usuario';
+      }
+    });
+  }
 }
