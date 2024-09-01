7be11712c9a77491e762c380ac954e4626e9c8c4
Carlos Peña <cgabriel2303@gmail.com>
2024-08-31 20:47:05 -0400

    add logout

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,4 +9,15 @@ import { Component, Input } from '@angular/core';
 })
 export class MainMenuComponent {
   @Input() userName: string='';
+
+  constructor(private afAuth: AngularFireAuth,
+    private router: Router
+  ){}
+  logout() {
+    this.afAuth.signOut().then(() => {
+      this.router.navigate(['/login']);
+      }).catch(error=>{
+        console.log('Error al cerrar sesion ',error);
+      });
+  }
 }
